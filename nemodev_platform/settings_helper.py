824055fbd0bf39bbd359fb6f7d50c630c19223d4
nemodeveloper <dsytujnjds1>
2017-01-02 22:26:27 +0300

    Исправлен путь к файлу конфигурации

--- nemodev_platform/settings_helper.py
+++ nemodev_platform/settings_helper.py
@@ -3,4 +3,4 @@ import socket
 from nemodev_platform.settings_prod import ALLOWED_HOSTS
 
 PRODUCTION = True if socket.gethostname() in ALLOWED_HOSTS else False
-SETTINGS_MODULE = 'nemodev_platform.settings_prod' if PRODUCTION else 'nemode_platform.settings_dev'
+SETTINGS_MODULE = 'nemodev_platform.settings_prod' if PRODUCTION else 'nemodev_platform.settings_dev'
